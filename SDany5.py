81f21f64fe1be074c8f7373b026e6aab8b2009b9
okichan06 <xuncangjing704@gmail.com>
2023-12-09 21:34:16 +0900

    connected SD.py with tr.py

--- SDany5.py
+++ SDany5.py
@@ -1,14 +1,15 @@
 from diffusers import StableDiffusionPipeline, EulerAncestralDiscreteScheduler
 from safetensors import safe_open
 import time
+from translate import create_prompt
 
-def create_image():
+def create_image(Input_prompt):
     model_id = "./model/AnythingV5Ink_v5PrtRE.safetensors"
     pipe = StableDiffusionPipeline.from_single_file(model_id)
     pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
     # pipe.to("cuda")
     # # pipe.enable_attention_slicing()
-    prompt = "masterpiece, best quality, 1man, blue hair, wolf ears, looking at viewer, :3, scarf, jacket, outdoors, forest, slanted eyes"
+    prompt = create_prompt(Input_prompt)
     negative_prompt = " (deformed iris, deformed pupils, semi-realistic, cgi, 3d, render, sketch, cartoon, drawing, anime:1.4), text, close up, cropped, out of frame, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck,NSFW"
     image = pipe(
         prompt,
--- translate.py
+++ translate.py
@@ -1,13 +1,13 @@
 from googletrans import Translator
 
-def create_prompt():
+def create_prompt(Input_prompt):
     prompt = "(((super realistic))), (((best quality))),((masterpiece)), ((ultra-detailed)), an anime style"
     translator = Translator()
-    character = prompt
+    character = Input_prompt
     jp_words = [character]
     en_words = []
     
     for src in jp_words:
-    dst = translator.translate(src, src='ja', dest='en')
-    en_words.append(dst.text)
-    prompt = prompt + ', '.join(en_words)
+        dst = translator.translate(src, src='ja', dest='en')
+        en_words.append(dst.text)
+        prompt = prompt + ', '.join(en_words)
