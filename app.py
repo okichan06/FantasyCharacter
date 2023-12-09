2ebe87f49ce3e618e9a58176569f3db7923abf30
okichan06 <xuncangjing704@gmail.com>
2023-12-09 16:47:34 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__) 
+
+@app.route('/',methods=['POST'])
+def prompt():
+    return render_template('index.html')
+
+@app.route('/result')
+def result():
+    #この後画像を生成
+    message = '準備中です。ちょっと待ってね'
+    return render_template('result.html',msg=message)
+
+if __name__ == "__main__": 
+    app.run(debug=True)
