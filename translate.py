ec4e24e99c77880a36d375be3c97e8cc8e4d6825
okichan06 <xuncangjing704@gmail.com>
2023-12-09 17:51:27 +0900

    add translate.py file

---
+++ translate.py
@@ -0,0 +1,13 @@
+from googletrans import Translator
+
+def create_prompt():
+    prompt = "(((super realistic))), (((best quality))),((masterpiece)), ((ultra-detailed)), an anime style"
+    translator = Translator()
+    character = prompt
+    jp_words = [character]
+    en_words = []
+    
+    for src in jp_words:
+    dst = translator.translate(src, src='ja', dest='en')
+    en_words.append(dst.text)
+    prompt = prompt + ', '.join(en_words)
