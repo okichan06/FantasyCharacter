296f8ed28d04603937e6122b365710a62fcd3d01
okichan06 <xuncangjing704@gmail.com>
2023-12-12 21:02:24 +0900

    error checking

--- app.py
+++ app.py
@@ -5,18 +5,21 @@ from translate import create_prompt
 app = Flask(__name__) 
 
 @app.route('/')
-def prompt():
+def index():
     return render_template('index.html')
 
 @app.route('/result',methods=['GET','POST'])
 def result():
+    A=0
     if request.method == "GET":
         return render_template('result.html')
     elif request.method == "POST":
         Input_prompt = request.form['Input_prompt']
         prompt = create_prompt(Input_prompt)
-        result_img = create_image(prompt)
-        return render_template('result.html',result = result_img)
+        A=1
+        #result_img = create_image(prompt)
+        #return render_template('result.html',result = result_img)
+        return render_template('result.html',prompt=prompt,A=A)
 
 if __name__ == "__main__": 
     app.run(debug=True)
--- translate.py
+++ translate.py
@@ -1,13 +1,19 @@
 from googletrans import Translator
 
 def create_prompt(Input_prompt):
+    print("start translating")
     prompt = "(((super realistic))), (((best quality))),((masterpiece)), ((ultra-detailed)), an anime style"
     translator = Translator()
     character = Input_prompt
     jp_words = [character]
     en_words = []
+    print("I'm ready!")
     
     for src in jp_words:
         dst = translator.translate(src, src='ja', dest='en')
         en_words.append(dst.text)
         prompt = prompt + ', '.join(en_words)
+    
+    if __name__ == "__main__":
+        create_prompt()
+        print(prompt)
